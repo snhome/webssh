a8610eb9ac68180a2250f6edf28043a0653c0537
jack <ameba.jack@gmail.com>
2022-08-18 01:57:43 +0800

    add job

--- mongodb.py
+++ mongodb.py
@@ -10,11 +10,24 @@ print(db_url)
 myclient = pymongo.MongoClient(db_url)
 mydb = myclient["webssh"]
 
-mycol = mydb["ssh_servers"]
+ssh_servers = mydb["ssh_servers"]
+ssh_servers.create_index('id', unique=True)
 
+home_server_dict = {"hostname": "192.168.1.145", "port":22, "username": "root", "password": "", 'id': 'home-145', 'agent': True }
 
-mydict = { "name": "John", "address": "Highway 37", 'nid': generate(size=11) }
+try:
+  x = ssh_servers.insert_one(home_server_dict)
+except Exception as e:
+  print(e)
 
-x = mycol.insert_one(mydict)
-print(x.inserted_id)
-print(mydict)
+ssh_jobs = mydb["ssh_jobs"]
+ssh_jobs.create_index('id', unique=True)
+
+jobid = generate(size=11)
+job_dict = { 'command': 'echo hi\nsleep 2\necho done', 'id': jobid, 'server_id': home_server_dict['id']}
+x = ssh_jobs.insert_one(job_dict)
+
+job = ssh_jobs.find_one({'id':jobid})
+server = ssh_servers.find_one({'id': job['server_id']})
+
+print(server['hostname'])
